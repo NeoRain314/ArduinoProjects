0686a737e4f283dadf419b8ab0a4e074a758e474
NeoRain314 <jasmin.dammertz@googlemail.com>
2024-04-17 17:40:37 +0200

    remade the code to look more clear

--- NwT_Projects/VitaldatenProjekt/konzentrationsSpiel_NwT/konzentrationsSpiel_NwT.ino
+++ NwT_Projects/VitaldatenProjekt/konzentrationsSpiel_NwT/konzentrationsSpiel_NwT.ino
@@ -13,7 +13,7 @@ char hexaKeys[ROWS][COLS] = {
   {'3', '2', '1'}
 };
 
-int order[5] = {0, 0, 0, 0, 0};
+
 
 byte colPins[COLS] = { 8, 7, 6 }; //Definition der Pins für die 3 Spalten
 byte rowPins[ROWS] = { 5, 4, 3, 2 };//Definition der Pins für die 4 Zeilen
@@ -21,27 +21,74 @@ byte rowPins[ROWS] = { 5, 4, 3, 2 };//Definition der Pins für die 4 Zeilen
 char Taste; //pressedKey entspricht in Zukunft den gedrückten Tasten
 Keypad myKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); //Das Keypad kann absofort mit myKeypad angesprochen werden
 
-int stat = 0; // 0 -> neue zufallszahl; 1 -> warten bis rand taste gedrückt;2 -> warten bis nächste taste in array gedrückt
-int rando = 0;
-int tasteSave = 0;
-int index = 0; //index in order arr
 
-// -----------------------------------------------  //
+int stat = 0; // 0 -> neue zufallszahl; 1 -> warten bis rand taste gedrückt;2 -> warten bis nächste taste in array gedrückt
+
+//order array
+int order[5] = {0, 0, 0, 0, 0};
+int order_index = 0; //index in order arr
+int order_length;
+int game_round = 0;
+int rando = 0; //
+
+// <-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><->  //
 
 void setup() {
+  //initializing --------------------
   Serial.begin(9600);
+
   int seed = analogRead(A0);
   randomSeed(seed);
   Serial.print("Seed: ");
   Serial.println(seed);
+
+  //array --------------------
+  order_length = sizeof(order) / sizeof(order[0]);
+  for(int i=0; i<order_length; i++) {
+    order[i] = random(1, 10);
+  }
+
+  /*for(int i=0; i<order_length; i++) {
+    Serial.println(order[i]);
+  }*/
 }
 
-// -----------------------------------------------  //
+// <-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><->  //
 
 void loop() {
   //Taste = myKeypad.getKey();
   //Serial.println(Taste);
+  if(order_index == game_round){
+    if(game_round == order_length){
+      Serial.println("Finished");
+    }else{
+      order_index = 0;
+      game_round++;      
+    }
+    
+    
+  } else {
+    Taste = myKeypad.getKey();
+    Serial.println(order[order_index]);
+    if (Taste == ('0' + order[order_index])) {
+      Serial.println("right button");
+      order_index++;
+    }
+  }
+  
+}
 
+
+
+
+
+
+
+
+
+// <-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><->  //
+
+/*  --- old code --
   if(stat == 0){
     //save new number in array
     rando = random(1, 10);
@@ -68,10 +115,10 @@ void loop() {
       stat = 0; //neue zufallszahl
     } 
   }
-  
-}
 
-void saveInArr(int x, int arr[]){
+
+
+  void saveInArr(int x, int arr[]){
   for(int i=0; i< (sizeof(arr) / sizeof(arr[0])); i++){
     if(arr[i]){ // kann man das so machen? Keine ahnung?
       //... idk what, nothing...?!
@@ -81,7 +128,7 @@ void saveInArr(int x, int arr[]){
     }
   }
 }
-
+  */
 
 /*
 if (Taste) { //Wenn eine Taste gedrückt wurde
@@ -91,3 +138,8 @@ if (Taste) { //Wenn eine Taste gedrückt wurde
       Serial.println(); //Teile uns am Serial Monitor die gedrückte Taste mit
     }
 */
+
+
+
+
+
