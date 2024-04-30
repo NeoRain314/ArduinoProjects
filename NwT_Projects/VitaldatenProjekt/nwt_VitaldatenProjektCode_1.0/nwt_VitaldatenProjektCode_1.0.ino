65cfc15bce8f60aabcdbf2060f0957b47d022c8a
NeoRain314 <jasmin.dammertz@googlemail.com>
2024-04-30 17:29:34 +0200

    replaced final code with rigth code

--- NwT_Projects/VitaldatenProjekt/nwt_VitaldatenProjektCode_1.0/nwt_VitaldatenProjektCode_1.0.ino
+++ NwT_Projects/VitaldatenProjekt/nwt_VitaldatenProjektCode_1.0/nwt_VitaldatenProjektCode_1.0.ino
@@ -19,9 +19,10 @@ Bugs:
 #include <Key.h>
 #include <Keypad.h>
 
+
 // ------ main ----------------------------------------------------
 #define TASTER_PIN 18
-int mode = 0; //0 -> Konzentrationsspiel; 1 -> Puls & Temp
+int mode = 0; //0 -> start; 1 --> Konzentrationsspiel; 2 -> Puls & Temp
 volatile int taster_stat = 0;
 
 // ------ LCD Display ----------------------------------------------
@@ -30,11 +31,17 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 // ------ temperatursensor -----------------------------------------
 #define TEMP_SENSOR_PIN A0
 int temp_value = 0;
-int temperatur = 0;
+float temperatur = 0;
 
 // ------ pulssensor -----------------------------------------------
-#define PULS_SENSOR_PIN A1
+#define PULS_SENSOR_PIN A3
 int puls_value = 0;
+int puls = 0;
+
+
+// ------ konzentrationsspiel -----------------------------------------------
+bool gamestart = false; //
+
 
 // <-> Setup <-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-> Setup <->  //
 
@@ -48,15 +55,28 @@ void setup() {
 
   //main ----------------------------------------------------
   pinMode(TASTER_PIN, INPUT_PULLUP);  
+
+
+  // start screen
+  startMode();
 }
 
 
 // <-> Loop <-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-><-> Loop <->  //
 
 void loop() {
+  
   if(taster_stat == 1){
     taster_stat = 0;
     changeMode();
+  }
+
+  if(mode == 1){ //temp- & pulssensor
+    //sensors();
+  }else if(mode == 2){ //konzentrationsspiel
+    if(gamestart == true){
+      //konzentrationsspiel();
+    }
   }
 }
 
@@ -71,37 +91,54 @@ void tasterInterrupt(){
 }
 
 void changeMode(){
-  if(mode < 1){
+  if(mode < 2){
     mode++;
   }else {
-    mode = 0;
+    mode = 1;
   }
   //Serial.println(mode);
-  useMode();
+  startMode();
 }
 
-void useMode(){
-  if(mode == 0){ //temp- & pulssensor
+void startMode(){
+  if(mode == 0){ //start screen
+    Serial.println(">-------------------- Vitaldaten Projekt NwT Klasse 9 --------------------<");
+    lcd.clear();
+    printCharLcd("Vitaldatenprojekt", 0, 0);
+    printCharLcd("NwT Klasse 9", 1, 0);
+  }else if(mode == 1){ //temp- & pulssensor
+    Serial.println("Vitaldatenmessung");
     lcd.clear();
     printCharLcd("Vitaldaten", 0, 0);
     printCharLcd("Messung", 1, 0);
-    sensor();
-  }else if(mode == 1){ //konzentrationsspiel
+
+    delay(2000);
+  }else if(mode == 2){ //konzentrationsspiel
+    Serial.println("Konzentrationsspiel");
     lcd.clear();
     printCharLcd("Konzentration", 0, 0);
     printCharLcd("Spiel", 1, 0);
 
-    konzentrationsspiel();
+    gamestart = true;
   }
 }
 
-void konzentrationsspiel(){
-  Serial.println("Konzentrationsspiel");
+void sensors(){
+  tempSensor();
+  pulsSensor();
+
+  lcd.clear();
+  printCharLcd("Temperatur:", 0, 0);
+  printFloatLcd(temperatur, 0, 11);
+  printCharLcd("Puls:", 1, 0);
+  printNumLcd(puls, 1, 5);
 }
 
-void sensor(){
-  Serial.println("Vitaldatenmessung");
+void konzentrationsspiel(){
+  gamestart = false;
 }
+
+
 
 // ~~~ all ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ all ~~~~~~~~~~~~~~
 
@@ -117,17 +154,29 @@ void printNumLcd(int num, int cursorX, int cursorY){
   lcd.print(num);
 }
 
+void printFloatLcd(float num, int cursorY, int cursorX){
+  //Lcd.clear();
+  lcd.setCursor(cursorX, cursorY);
+  lcd.print(num);
+}
 // ~~~ Konzentrationsspiel ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Konzentrationsspiel ~~~
 
 
 // ~~~ Pulssensor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Pulssensor ~~~~~~~~~~~~~~~
 
+void pulsSensor(){
+  puls = 0;
+}
 
 // ~~~ Temperatursensor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Temperatursensor ~~~~~~~~~~~~~~~
 
-void temperaturMessung(){
+void tempSensor(){
   temp_value = analogRead(TEMP_SENSOR_PIN);
-  temperatur = map(temp_value,51,41,238,206);
+  temp_value /= 10;
+  temperatur = map(temp_value, 42, 20, 215, 99); 
+  temperatur /= 10;
+  
   Serial.println(temperatur);
+  delay(500);
 }
 
