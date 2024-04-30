1a4e6f66058a67c48a79696e6a3c6cc0777465e1
NeoRain314 <jasmin.dammertz@googlemail.com>
2024-04-30 17:41:32 +0200

    worked on fixing mode bug, not finished

--- NwT_Projects/VitaldatenProjekt/nwt_VitaldatenProjektCode_1.0/nwt_VitaldatenProjektCode_1.0.ino
+++ NwT_Projects/VitaldatenProjekt/nwt_VitaldatenProjektCode_1.0/nwt_VitaldatenProjektCode_1.0.ino
@@ -9,7 +9,8 @@ Todo:
  > ...
 
 Bugs:
- > ...
+ > mode everytime 1 at beginning (should be 0 till button press)
+ > overwrites "Vitaldatenmessung" almost everytime with "Konzentrationsspiel" (--> mode!!)
 
 */
 
@@ -19,14 +20,32 @@ Bugs:
 #include <Key.h>
 #include <Keypad.h>
 
+// ------ Keypad ----------------------------------------
+//defines Size of Keypad
+const byte COLS = 3; //3 Spalten
+const byte ROWS = 4; //4 Zeilen; (schwarz)
+
+char hexaKeys[ROWS][COLS] = { //Die Ziffern/Zeichen
+  {'#', '0', '*'},
+  {'9', '8', '7'},
+  {'6', '5', '4'},
+  {'3', '2', '1'}
+};
+
+byte colPins[COLS] = { 8, 7, 6 }; //Definition der Pins für die 3 Spalten
+byte rowPins[ROWS] = { 5, 4, 3, 2 };//Definition der Pins für die 4 Zeilen
+
+char Taste; //pressed Key
+Keypad myKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+// ------ LCD Display ----------------------------------------------
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 
 // ------ main ----------------------------------------------------
 #define TASTER_PIN 18
 int mode = 0; //0 -> start; 1 --> Konzentrationsspiel; 2 -> Puls & Temp
 volatile int taster_stat = 0;
-
-// ------ LCD Display ----------------------------------------------
-LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // ------ temperatursensor -----------------------------------------
 #define TEMP_SENSOR_PIN A0
@@ -37,6 +56,24 @@ float temperatur = 0;
 #define PULS_SENSOR_PIN A3
 int puls_value = 0;
 int puls = 0;
+
+//------ konzentrationsspiel ----------------------------------------
+int order[30] = {}; //later with for 0 in array
+int order_index = 0; //index in order arr
+int order_length; //length of order array
+int game_round = 0; //current game round
+int led_stat = 0; //stat if leds were shown or not
+bool gameover = false;
+
+#define CTRL_LED 13
+#define BUZZ_PIN 12
+
+#define arr_length(a) (sizeof(a) / sizeof(a[0]))
+
+// ------ Sounds ----------------------------------------
+int sound_gameRight[1] = {440};
+int sound_gameover[4] = {210, 190, 210, 130};
+int sound_gameRoundFinish[1] = { 587};
 
 
 // ------ konzentrationsspiel -----------------------------------------------
@@ -58,7 +95,7 @@ void setup() {
 
 
   // start screen
-  startMode();
+  startScreen();
 }
 
 
@@ -94,19 +131,14 @@ void changeMode(){
   if(mode < 2){
     mode++;
   }else {
-    mode = 1;
+  mode = 1;
   }
-  //Serial.println(mode);
+  Serial.println(mode);
   startMode();
 }
 
 void startMode(){
-  if(mode == 0){ //start screen
-    Serial.println(">-------------------- Vitaldaten Projekt NwT Klasse 9 --------------------<");
-    lcd.clear();
-    printCharLcd("Vitaldatenprojekt", 0, 0);
-    printCharLcd("NwT Klasse 9", 1, 0);
-  }else if(mode == 1){ //temp- & pulssensor
+  if(mode == 1){ //temp- & pulssensor
     Serial.println("Vitaldatenmessung");
     lcd.clear();
     printCharLcd("Vitaldaten", 0, 0);
@@ -138,7 +170,12 @@ void konzentrationsspiel(){
   gamestart = false;
 }
 
-
+void startScreen(){
+  Serial.println(">-------------------- Vitaldaten Projekt NwT Klasse 9 --------------------<");
+  lcd.clear();
+  printCharLcd("Vitaldatenprojekt", 0, 0);
+  printCharLcd("NwT Klasse 9", 1, 0);
+}
 
 // ~~~ all ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ all ~~~~~~~~~~~~~~
 
