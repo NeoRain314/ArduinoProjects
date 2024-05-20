ef13a092b4d9c391a3c56d5262835f55e6bfefd7
NeoRain314 <jasmin.dammertz@googlemail.com>
2024-05-20 20:01:38 +0200

    calibrated temperature sensor

--- NwT_Projects/VitaldatenProjekt/nwt_temperatursensor/nwt_temperatursensor.ino
+++ NwT_Projects/VitaldatenProjekt/nwt_temperatursensor/nwt_temperatursensor.ino
@@ -1,20 +1,20 @@
-int sensor=A0;
-
+int sensor = A0;
 int messwert;
-
 float temperatur;
 
 void setup() {
-Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-messwert=analogRead(sensor);
-
-temperatur=map(messwert,51,41,238,206);
-
-Serial.println(temperatur);
-
-delay(200);
+  messwert = analogRead(sensor);
+  temperatur = map(messwert, 29, 79, 16, 40);
+  Serial.print(temperatur);
+  Serial.println(" C°");
+  delay(200);
 }
 
+
+// -- old map -- // map(messwert, 51, 41, 238, 206);
+
+// -- correct map -- // map(messwert, 29, 79, 16, 40);
